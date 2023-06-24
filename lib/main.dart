37c81c8a3984055c24dde3241a85341e82c65b62
Johannes Rantapää <johannes.rantapaa@gmail.com>
2023-06-24 18:18:15 +0300

    add multidex

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
